fc3600f10dafaaefe39de797538d9c2eff96a385
NikitaP2001 <generalhammond16@gmail.com>
2020-10-03 20:42:06 +0300

    Homework3

---
+++ Homework3/Task2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace ConsoleApp1
+{
+    abstract class Student
+    {
+        public Student(string n) { name = n; state = ""; }  
+        public void Read() {
+            state += "Read";                                
+        }
+        public void Write()
+        {
+            state += "Write";
+        }
+        public void Relax()
+        {
+            state += "Relax";
+        }
+        public abstract void Study();
+        public string name;
+        public string state;
+    }
+    class GoodStudent : Student
+    {
+        public GoodStudent(string n)
+        : base(n) { state = "Good";  }
+        public override void Study() {
+            Read();
+            Write();
+            Read();
+            Write();
+            Relax();
+        }
+    }
+    class BadStudent : Student  
+    {
+        public BadStudent(string n)
+        : base(n) { state = "Bad"; }
+        public override void Study() {
+            Relax();
+            Relax();
+            Relax();
+            Relax();
+        }
+    }
+    class Group
+    {
+        string Name;
+        List<Student> students = new List<Student>();
+        public Group (string GroupName) { Name = GroupName; }
+        public void AddStudent(Student st)
+        {
+            students.Add(st);
+        }
+        public string GetInfo()
+        {
+            string temp = "";
+            temp += "Group: " + Name + " Students: ";
+            foreach (Student st in students)
+            {
+                temp += st.name + " ";
+            }
+            return temp;
+        }
+        public string GetFullInfo()
+        {
+            string temp = "";
+            temp += "Group: " + Name + " Students: ";
+            foreach (Student st in students)
+            {
+                temp += st.state + " " + st.name + " ";
+            }
+            return temp;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Group gr = new Group("gr"); // Create group
+            BadStudent a = new BadStudent("stud1");  // Create students
+            GoodStudent b = new GoodStudent("stud2");
+            gr.AddStudent(a); // Adding students to a group
+            gr.AddStudent(b);
+            Console.WriteLine(gr.GetInfo() + "\n" + gr.GetFullInfo());
+        }
+    }
+}
---
+++ Homework3/Task3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Converter
+{
+    public Converter(double usd, double eur)
+    {
+        dollarmult = usd;
+        euromult = eur;
+    }
+    public double FromEuro(double count)
+    {
+        return Math.Round(count * euromult,2);
+    }
+    public double ToEuro(double count)
+    {
+        return Math.Round(count / euromult, 2);
+    }
+    public double FromDollar(double count)
+    {
+        return Math.Round(count * dollarmult, 2);
+    }
+    public double ToDollar(double count)
+    {
+        return Math.Round(count / dollarmult, 2);
+    }
+    double dollarmult;
+    double euromult;
+}
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter Convert = new Converter(28.32, 33.26);
+            Console.WriteLine("to euro " + Convert.ToEuro(1.04) + "\n" +
+                              "from euro " + Convert.FromEuro(5) + "\n" +
+                              "to dollar " + Convert.ToDollar(4) + "\n" +
+                              "from dollar " + Convert.FromDollar(6) + "\n"
+                );
+        }
+    }
+}
